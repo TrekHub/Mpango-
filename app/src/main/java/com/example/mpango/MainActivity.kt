e04d15c96a2383bfc62fd02dbec66b684483c6f1
TrekHub <teddyomondi001@gmail.com>
2022-06-28 21:23:29 +0300

    initial commit

---
+++ app/src/main/java/com/example/mpango/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.mpango
+
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
